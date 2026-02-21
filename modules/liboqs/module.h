b665c6f95567d321d47989872f681055442588e8
obi-tech <55135531+obi-tech@users.noreply.github.com>
2026-02-21 15:24:05 +0200

    Update the liboqs module header

--- modules/liboqs/module.h
+++ modules/liboqs/module.h
@@ -11,6 +11,11 @@ class liboqs : public Module {
     public:
         liboqs(void);
         std::optional<component::Digest> OpDigest(operation::Digest& op) override;
+        
+        // KEM operations
+        std::optional<component::KEM_KeyPair> OpKEM_GenerateKeyPair(operation::KEM_GenerateKeyPair& op) override;
+        std::optional<component::KEM_Encapsulated> OpKEM_Encapsulate(operation::KEM_Encapsulate& op) override;
+        std::optional<component::KEM_SharedSecret> OpKEM_Decapsulate(operation::KEM_Decapsulate& op) override;
 };
 
 } /* namespace module */
