c6daaae2f703d2b8e4347c3b6379e7fc137c42ac
obi-tech <55135531+obi-tech@users.noreply.github.com>
2026-03-15 07:40:01 +0200

    Removed unused OpDigest from module.h

--- modules/liboqs/module.h
+++ modules/liboqs/module.h
@@ -10,8 +10,7 @@ namespace module {
 class liboqs : public Module {
     public:
         liboqs(void);
-        std::optional<component::Digest> OpDigest(operation::Digest& op) override;
-        
+
         // KEM operations
         std::optional<component::KEM_KeyPair> OpKEM_GenerateKeyPair(operation::KEM_GenerateKeyPair& op) override;
         std::optional<component::KEM_Encapsulated> OpKEM_Encapsulate(operation::KEM_Encapsulate& op) override;
