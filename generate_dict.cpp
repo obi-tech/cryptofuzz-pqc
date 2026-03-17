3e2d4f98de864756325e27b6f5b96255d0acad45
obi-tech <55135531+obi-tech@users.noreply.github.com>
2026-03-17 12:55:53 +0200

    Added MLDSA dictionary entries

--- generate_dict.cpp
+++ generate_dict.cpp
@@ -61,6 +61,7 @@ int main(void)
     writeMap(fp, ECC_CurveLUTMap);
     writeMap(fp, CalcOpLUTMap);
     writeMap(fp, KEMLUTMap);
+    writeMap(fp, MLDSALUTMap);
     {
         const std::vector<uint8_t> bufferSizes = {1, 2, 4, 8, 12, 16, 32};
         for (const auto& size : bufferSizes) {
