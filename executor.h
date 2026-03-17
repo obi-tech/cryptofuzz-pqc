c2a63e3b5ff889c960286065e133aad7ffd77a1c
obi-tech <55135531+obi-tech@users.noreply.github.com>
2026-03-17 12:47:25 +0200

    Declared executor type aliases for MLDSA operations

--- executor.h
+++ executor.h
@@ -294,5 +294,8 @@ using ExecutorSR25519_Verify = ExecutorBase<bool, operation::SR25519_Verify>;
 using ExecutorKEM_GenerateKeyPair = ExecutorBase<component::KEM_KeyPair, operation::KEM_GenerateKeyPair>;
 using ExecutorKEM_Encapsulate = ExecutorBase<component::KEM_Encapsulated, operation::KEM_Encapsulate>;
 using ExecutorKEM_Decapsulate = ExecutorBase<component::KEM_SharedSecret, operation::KEM_Decapsulate>;
+using ExecutorMLDSA_GenerateKeyPair = ExecutorBase<component::MLDSA_KeyPair, operation::MLDSA_GenerateKeyPair>;
+using ExecutorMLDSA_Sign = ExecutorBase<component::MLDSA_Signature, operation::MLDSA_Sign>;
+using ExecutorMLDSA_Verify = ExecutorBase<bool, operation::MLDSA_Verify>;
 
 } /* namespace cryptofuzz */
